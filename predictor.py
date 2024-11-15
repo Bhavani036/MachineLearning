a98117f900f3262fedd9237048684ec412102324
Bhavani036 <83528978+Bhavani036@users.noreply.github.com>
2024-11-15 12:48:45 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,7 +1,17 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+import requests
 import pickle
 
-with open('model_pickle2', 'rb') as f:
-  model = pickle.load(f)
+model_url="https://raw.githubusercontent.com/Bhavani036/MachineLearning/master/model_pickle2"
+
+response = requests.get(model_url,stream=True)
+response.raise_for_status()
+
+model = pickle.load(response.content)
 
 
 def predict_house_price(area, bathrooms, age):
